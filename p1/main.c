43ee9d72941fc5b6cd57622341f8542b0f085be9
2025050193 <youngpoptest2080@gmail.com>
2026-04-15 16:54:51 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -3,26 +3,26 @@
 int main(void)
 {
     int n=0, ans=0, total=0;
-    scanf("%d", &ans);
+    scanf("%d", &ans); //정답 하나 먼저 입력 받기
     
     do {
-        scanf("%d", &n);
+        scanf("%d", &n); // 맞힐 때까지 정수 입력 받기
         
-        if (ans>n) {
+        if (ans>n) { // if문을 통해 정답과 입력 받은 정수 사이의 크기를 비교, 정답이 입력 받은 정수보다 작을 때
             printf("%d<?\n",n, n);
-            total+=1;
+            total+=1; //시도 횟수 카운트
         }
-        else if (ans<n) {
+        else if (ans<n) { //정답이 입력 받은 정수보다 클 때
             printf("%d>?\n", n, n);
-            total+=1;
+            total+=1; //시도 횟수 카운트
         }
         else {
-            printf("%d==?\n", n,n);
-            total+=1;
+            printf("%d==?\n", n,n); //정답과 입력 값이 같을 때
+            total+=1; // 시도 횟수 카운트
             printf("%d",total, total);
             break;
         }
-    } while(ans!=n);
+    } while(ans!=n); // 정답과 입력 받은 정수가 다를때 계속 반복, 같아지면 break
     
 
     return 0;
